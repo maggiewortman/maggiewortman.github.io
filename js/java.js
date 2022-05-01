ccf1186a0b4d329c25765bd571a032285ab7dec6
maggiewortman <mawo0447@colorado.edu>
2022-05-01 14:36:41 -0600

    push

--- js/java.js
+++ js/java.js
@@ -1,29 +1,38 @@
+const para = document.querySelector('h1');
+
+para.addEventListener('click', updateName);
+
+function updateName () {
+  const name = prompt('Enter a new name');
+  para.textContent = 'On Repeat Today: ' + name;
+}
+
 const n = 5;
 const epicMix = [
-  "The Complete Knock",
-  "Rocky",
-  "ALL THESE CHANGES",
-  "The Morning",
-  "We Are The People",
-  "Loud Places",
-  "Billy",
-  "Honey",
-  "Back To What I Know",
-  "Confessions",
-  "By Design",
-  "Is It True",
-  "So Good At Being in Trouble",
-  "Sundress",
-  "Bambi",
-  "White Ferrari",
-  "HOTTIE",
-  "Let Go",
-  "Too Good",
-  "Menswear",
-  "The Divine Chord",
-  "Life Itself",
-  "peppermint",
-  "Baby Baby"
+  "The Complete Knock - Blood Orange",
+  "Rocky - Still Woozy",
+  "ALL THESE CHANGES - Nick Hakim",
+  "The Morning - The Weeknd",
+  "We Are The People - Empire of the Sun",
+  "Loud Places - Jaime xx",
+  "Billy - DRAMA",
+  "Honey - 070 Shake",
+  "Back To What I Know - NEIL FRANCES",
+  "Confessions - Sudan Archieves",
+  "By Design - Kid Cudi",
+  "Is It True - Tame Impala",
+  "So Good At Being in Trouble - Unknown Mortal Orchestras",
+  "Sundress - A$AP Rocky",
+  "Bambi - Hippo Campus",
+  "White Ferrari - Frank Ocean",
+  "HOTTIE - Brockhampton",
+  "Let Go - RAC",
+  "Too Good - Drake",
+  "Menswear - The 1975",
+  "The Divine Chord - The Avalanches",
+  "Life Itself - Glass Animals",
+  "peppermint - lavender",
+  "Baby Baby - Sports"
 ]
   .map(x => ({ x, r: Math.random() }))
   .sort((a, b) => a.r - b.r)
@@ -48,3 +57,22 @@ const mixInfo = function (mix) {
     mixList.append(li);
   });
 };
+
+var index = 0;
+    change();
+
+    function change() {
+        var x = document.getElementsByClassName('slides');
+        for(var i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        index++;
+
+        if(index > x.length) {
+            index = 1;
+        }
+
+        x[index - 1].style.display = "block";
+
+        setTimeout(change, 3000);
+    }
